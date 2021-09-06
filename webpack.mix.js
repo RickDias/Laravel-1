12ebb8ac5c90dad74d28fdac3863fd3a28e2ccba
RickDias <rick_pereira_dias@hotmail.com>
2021-09-06 19:45:28 -0300

    jquery + bootstrap

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
-    .sourceMaps();
+mix
+
+.scripts('node_modules/jquery/dist/jquery.js' , 'public/site/jquery.js')
+.scripts('node_modules/bootstrap/dist/js/bootstrap.bundle.js','public/site/bootstrap.js')
+;
